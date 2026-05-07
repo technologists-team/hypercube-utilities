07fa281daaee8b7624f1ccd7e7e98d8cad0b8a67
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-05-08 04:24:12 +1000

    feat: IdStruct from hypercube engine

---
+++ src/Hypercube.Utilities.Attributes/IdStructAttribute.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace Hypercube.Utilities.Attributes;
+
+[AttributeUsage(AttributeTargets.Struct)]
+public sealed class IdStructAttribute : Attribute
+{
+    [UsedImplicitly] public readonly Type UnderlyingType;
+    [UsedImplicitly] public readonly bool NullCast;
+    
+    public IdStructAttribute(Type underlyingType, bool nullCast = false)
+    {
+        UnderlyingType = underlyingType;
+        NullCast = nullCast;
+    }
+}
